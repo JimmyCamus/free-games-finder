0f5c4a8b735ac5038683754c0ab94bb88fe31402
Jeremy-Camus-Varela-Aiquia <jeremy.camus.varela@gmail.com>
2024-02-09 17:37:34 -0300

    feat: list of games have been implemented

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 80px rgba(255, 255,255, 0.2)",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
