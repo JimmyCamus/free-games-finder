a9c42c3e2ba7a6f873f07a12a6670a469b050224
Jeremy-Camus-Varela-Aiquia <jeremy.camus.varela@gmail.com>
2024-02-13 11:33:10 -0300

    chore: react deps have been added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: netlify({
-    imageCDN: true,
-  }),
+    imageCDN: true
+  })
 });
